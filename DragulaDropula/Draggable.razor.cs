4c88a054ba6c7a3e9f13a89a76c4398c6cdb2def
Marat Gevorkyan <marat2255a@gmail.com>
2022-12-24 21:17:42 +0300

    Update version to 1.3.1. Decompose DropThis method by adding TryReturnBack method.

--- DragulaDropula/Draggable.razor.cs
+++ DragulaDropula/Draggable.razor.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using static DragulaDropula.DragNDropController;
 
 namespace DragulaDropula;
 
@@ -36,34 +37,52 @@ public class DraggableModel : ComponentBase
     /// <summary>
     /// Method will be invoked when user drops this component.
     /// </summary>
-    [Parameter] public Action<object, double, double>? OnDropWithPosition { get; set; }
+    [Parameter] public Action<object, int, int>? OnDropWithPosition { get; set; }
 
     /// <summary>
     /// X position. Set to 0 by default.
     /// </summary>
-    [Parameter] public double X { get; set; }
-    [Parameter] public EventCallback<double> XChanged { get; set; }
+    [Parameter] public int X { get; set; }
+    [Parameter] public EventCallback<int> XChanged { get; set; }
 
     /// <summary>
     /// Y position. Set to 0 by default.
     /// </summary>
-    [Parameter] public double Y { get; set; }
-    [Parameter] public EventCallback<double> YChanged { get; set; }
-
-#region CursorHandling
+    [Parameter] public int Y { get; set; }
+    [Parameter] public EventCallback<int> YChanged { get; set; }
 
     protected bool IsDragging { get; private set; }
 
     private double CursorX { get; set; }
     private double CursorY { get; set; }
 
-    protected void StartDragging(MouseEventArgs e) {
+    private void TryReturnBack()
+    {
+        if (!MustReturnBackOnDrop) return;
+        X = 0;
+        Y = 0;
+        XChanged.InvokeAsync(X);
+        YChanged.InvokeAsync(Y);
+    }
+
+    protected void StartDragging(MouseEventArgs e)
+    {
         IsDragging = true;
         CursorX = e.PageX;
         CursorY = e.PageY;
 
         MouseSrv.OnMove += MoveThis;
         MouseSrv.OnDrop += DropThis;
+    }
+
+    protected void StartDragging(MouseEventArgs e, DraggingEventHandler actionToMove, DroppingEventHandler actionToDrop)
+    {
+        IsDragging = true;
+        CursorX = e.PageX;
+        CursorY = e.PageY;
+
+        MouseSrv.OnMove += actionToMove;
+        MouseSrv.OnDrop += actionToDrop;
     }
 
     private void DropThis(out DraggableModel droppedItem)
@@ -77,20 +96,28 @@ public class DraggableModel : ComponentBase
         OnDrop?.Invoke(droppedItem);
         OnDropWithPosition?.Invoke(droppedItem, X, Y);
         
-        if (MustReturnBackOnDrop)
-        {
-            X = 0;
-            Y = 0;
-            XChanged.InvokeAsync(X);
-            YChanged.InvokeAsync(Y);
-        }
+        TryReturnBack();
     }
 
-    private void MoveThis(MouseEventArgs e) {
+    protected void DropThis(out DraggableModel droppedItem, DraggingEventHandler actionToMove, DroppingEventHandler actionToDrop)
+    {
+        IsDragging = false;
+        droppedItem = this;
+
+        MouseSrv.OnMove -= actionToMove;
+        MouseSrv.OnDrop -= actionToDrop;
+
+        OnDrop?.Invoke(droppedItem);
+        OnDropWithPosition?.Invoke(droppedItem, X, Y);
+
+        TryReturnBack();
+    }
+
+    protected void MoveThis(MouseEventArgs e) {
         if (!IsDragging) return;
 
-        X -= CursorX - e.PageX;
-        Y -= CursorY - e.PageY;
+        X -= (int)(CursorX - e.PageX);
+        Y -= (int)(CursorY - e.PageY);
         
         CursorX = e.PageX;
         CursorY = e.PageY;
@@ -98,6 +125,4 @@ public class DraggableModel : ComponentBase
         XChanged.InvokeAsync(X);
         YChanged.InvokeAsync(Y);
     }
-
-#endregion
 }
